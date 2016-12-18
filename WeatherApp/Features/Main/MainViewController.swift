e06de86fdcf0e40fcb8582017b9930ddd824e945
Ravi <ravi.randeria@nyu.edu>
2016-12-18 13:28:35 +0530

    Reorganized file structure within Xcode And Project Folder for ease of understanding

---
+++ WeatherApp/Features/Main/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  WeatherApp
+//
+//  Created by RAVI RANDERIA on 12/18/16.
+//  Copyright © 2016 RAVI RANDERIA. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
