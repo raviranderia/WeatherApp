000d8fdaa12a3852fdc3cd58ef1b480db46565dd
Ravi <ravi.randeria@nyu.edu>
2016-12-18 13:54:00 +0530

    added alamofire and swifty json pods to handle the networking layer and parsing of JSON Data

---
+++ WeatherApp/Networking/RequestManager.swift
@@ -0,0 +1,16 @@
+//
+//  RequestManager.swift
+//  WeatherApp
+//
+//  Created by RAVI RANDERIA on 12/18/16.
+//  Copyright © 2016 RAVI RANDERIA. All rights reserved.
+//
+
+import Foundation
+
+struct RequestManager {
+    
+    
+    
+    
+}
